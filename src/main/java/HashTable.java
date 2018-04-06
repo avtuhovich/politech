4a9593f072f540c9207673be6196c97879a7c7c1
avtuhovich.ns <ipfelps@gmail.com>
2018-04-06 20:56:17 +0200

    HashTable

---
+++ src/main/java/HashTable.java
@@ -0,0 +1,91 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class HashTable {
+    private int[] head;
+    private int[] next;
+    private int[] values;
+    private int cnt = 1;
+    private int headNumber;
+
+    /**
+     * Create HashTable
+     *
+     * @param headNumber - Count array pointer to int values corresponding to the same hash-value
+     * @param maxSize    - maxSize of table
+     */
+    public HashTable(int headNumber, int maxSize) {
+        this.headNumber = headNumber;
+        head = new int[headNumber];
+        next = new int[maxSize + 1];
+        values = new int[maxSize + 1];
+    }
+
+    /**
+     * adding a value
+     *
+     * @param x - value
+     */
+    public void add(int x) {
+        if (this.contains(x)) {
+            return;
+        }
+        int h = index(hash(x));
+        next[cnt] = head[h];
+        values[cnt] = x;
+        head[h] = cnt++;
+    }
+
+    /**
+     * removal value
+     *
+     * @param x - value
+     * @return
+     */
+    public boolean remove(int x) {
+        int h = index(hash(x));
+        for (int i = head[h]; i != 0; i = next[i]) {
+            if (values[i] == x) {
+                next[cnt] = 0;
+                head[h] = cnt--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Contains value in HashTable
+     *
+     * @param x - value
+     * @return
+     */
+    public boolean contains(int x) {
+        int h;
+        h = index(hash(x));
+        for (int i = 1; i != 0; i = next[i])
+            if (values[i] == x)
+                return true;
+        return false;
+    }
+
+    private int hash(int x) {
+        return Math.abs((x >> 15) ^ x);
+    }
+
+    private int index(int hash) {
+        return Math.abs(hash) % headNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        HashTable hashTable = (HashTable) o;
+
+        if (this.headNumber != headNumber) return false;
+        return true;
+    }
+}
---
+++ src/test/java/HashTableTest.java
@@ -0,0 +1,37 @@
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import main.java.HashTable;
+
+public class HashTableTest {
+
+    private HashTable hs;
+
+    @Test
+    public void add() {
+        hs = new HashTable(4, 5);
+        hs.add(5);
+        assertTrue(hs.contains(5));
+        assertFalse(hs.contains(4));
+    }
+
+    @Test
+    public void remove() {
+        hs = new HashTable(4, 5);
+        hs.add(5);
+        assertTrue(hs.contains(5));
+        hs.remove(5);
+        assertFalse(hs.contains(0));
+    }
+
+    @Test
+    public void contains() {
+        hs = new HashTable(4, 5);
+        hs.add(5);
+        assertTrue(hs.contains(5));
+        assertFalse(hs.contains(4));
+    }
+
+}
